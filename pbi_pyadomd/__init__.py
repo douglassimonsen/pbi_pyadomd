d49e643233c20fdf0281dcaefe24a9a21d46b893
gitea <a@a.com>
2025-08-09 13:16:13 -0500

    Updating a single stub method

--- pbi_pyadomd/__init__.py
+++ pbi_pyadomd/__init__.py
@@ -1,5 +1,5 @@
 from .conn import AdomdErrorResponseException, Connection, connect
 from .reader import Reader
 
-__version__ = "1.3.8"
+__version__ = "1.3.9"
 __all__ = ["AdomdErrorResponseException", "Connection", "Reader", "connect"]
